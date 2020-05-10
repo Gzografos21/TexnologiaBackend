359b56e029fe70e2715ef9b5d6896741ae64d76f
Loukia Dimou <loukiadimou1997@gmail.com>
2020-05-10 15:44:08 +0300

    Commit Staged

---
+++ BackendTexnologia/InvitedFriends.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class InvitedFriends
+    {
+        private string email;
+        private bool accepted;
+        private int earnedPointsInvitations;
+        private string sendingInvitationDate;
+    }
+}
---
+++ BackendTexnologia/Points.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class Points
+    {
+        private int collectedPoints;
+        private int pointsForBackpack;
+        private int pointsForDiscountedHostel;
+        private int pointsForFreeTrip;
+        private int redeemedPoints;
+        private int remainingPointsToBackpack;
+        private int remainingPointsToDiscountedHostel;
+        private int remainingPointsToFreeTrip;
+    }
+}
