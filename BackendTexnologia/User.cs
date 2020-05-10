2bff11bc8fed99d66f491c1ba46ee1614dd0aac8
Gzografos21 <63515820+Gzografos21@users.noreply.github.com>
2020-05-10 16:27:17 +0300

    Import User Back

---
+++ BackendTexnologia/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class User
+    {
+        private string name;
+        private string email;
+        private string password;
+        private string dateofbirth;
+        private string phone;
+    }
+}
