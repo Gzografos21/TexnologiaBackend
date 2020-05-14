68f13efc2cccf0fdb97fa5939a3041cf920eeabf
ezontou <62794584+ezontou@users.noreply.github.com>
2020-05-14 18:07:53 +0300

    Import Tickets

---
+++ BackendTexnologia/Tickets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class Tickets
+	{
+		private string placeOfDeparture;
+		private string placeOfArrival;
+		private string travelMethod;
+		private string company;
+		private double priceForDepartureTickets;
+		private double priceForArrivalTickets;
+		private int earnedPointsTickets;
+	}
+}
