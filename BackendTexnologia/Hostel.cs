0a4315847cd2d4d93882790344ad05a1d3f45ba1
ezontou <62794584+ezontou@users.noreply.github.com>
2020-05-14 18:06:07 +0300

    Import Hostel

---
+++ BackendTexnologia/Hostel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class Hostel
+	{
+		private string city;
+		private string country;
+		private int persons;
+		private double pricePerDay;
+		private int days;
+		private int earnedPointsHostel;
+	}
+}
