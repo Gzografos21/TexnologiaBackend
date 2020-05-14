00e0f7acbdae5339adb2c2be447214afae14619d
Valilo <58746615+Valilo@users.noreply.github.com>
2020-05-14 17:30:35 +0300

    Import Map

---
+++ BackendTexnologia/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class Map
+    {
+        private string tripDestination;
+        private string tripDate;
+        private bool pinPhoto;
+    }
+
+}
