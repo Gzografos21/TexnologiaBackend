6ac980b3ff2bc513c073521cfb688d0aee8d1dd2
loukiadimou <57858837+loukiadimou@users.noreply.github.com>
2020-05-14 17:51:25 +0300

    Import UploadedRoomsToFiloxenia

--- BackendTexnologia/UploadedRoomsToFiloxenia.cs
+++ BackendTexnologia/UploadedRoomsToFiloxenia.cs
@@ -7,5 +7,14 @@ namespace BackendTexnologia
 {
     public class UploadedRoomsToFiloxenia
     {
+        private string city;
+        private string country;
+        private int persons;
+        private double squareMeters;
+        private string startingDate;
+        private string endingDate;
+        private string information;
+        private bool photos;
+        private int earnedPointsUploadFiloxenia;
     }
 }
