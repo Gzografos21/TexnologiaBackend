1628356b6aca41b9cdf8b5ea28b7cd5d27edc69f
eliannadk <58879733+eliannadk@users.noreply.github.com>
2020-05-14 17:40:29 +0300

    import StudentPremium

---
+++ BackendTexnologia/StudentPremium.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class StudentPremium
+	{
+		private int studentId;
+	}
+}
