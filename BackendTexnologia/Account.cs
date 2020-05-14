bd0687c2f73ddb47044812ec5fe819c09b1e1cad
eliannadk <58879733+eliannadk@users.noreply.github.com>
2020-05-14 17:37:34 +0300

    import Account

---
+++ BackendTexnologia/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class Account
+	{
+		private string state;
+		private string dateOfUpgrade;
+		private string dateOfExpiration;
+		private double price;
+	}
+}
