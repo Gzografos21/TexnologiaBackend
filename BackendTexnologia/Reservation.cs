682116d6bf092760b67218ec06989cc465955e18
eliannadk <58879733+eliannadk@users.noreply.github.com>
2020-05-14 17:39:47 +0300

    import Reservation

---
+++ BackendTexnologia/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class Reservation
+	{
+		private string dateOfDeparture;
+		private string dateOfArrival;
+		private double totalAmountOfMoney;
+		private bool freeChangeToDates;
+	}
+}
