cb9ae6e6a22d56d64c0195e67a65c9c4089127ca
eliannadk <58879733+eliannadk@users.noreply.github.com>
2020-05-14 17:41:00 +0300

    import TravelInsurance

---
+++ BackendTexnologia/TravelInsurance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class TravelInsurance
+	{
+		private string insuranceType;
+		private double price;
+		private bool freeInsurance;
+	}
+}
