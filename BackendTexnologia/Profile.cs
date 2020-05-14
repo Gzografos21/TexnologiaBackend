9304571be893c11517d2047628ad497c90755796
Valilo <58746615+Valilo@users.noreply.github.com>
2020-05-14 17:41:12 +0300

    Import Profile

---
+++ BackendTexnologia/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class Profile
+    {
+        private bool profilePhoto;
+        private string placeOfLiving;
+        private int numberOfPlacesVisited;
+        private string dateOfLastJourney;
+        private string description;
+        private int numberOfPosts;
+        private int numberOfFavorites;
+    }
+}
