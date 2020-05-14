4173c13e68ec4cb553aeb39e3b328a324d7883d3
ezontou <62794584+ezontou@users.noreply.github.com>
2020-05-14 18:02:58 +0300

    Add files via upload

---
+++ BackendTexnologia/RoomsToFiloxenia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+	public class RoomsToFiloxenia
+	{
+		private string city;
+		private string country;
+		private string ownerName;
+		private int persons;
+		private int days;
+		private int earnedPointsReservationFiloxenia;
+	}
+}
