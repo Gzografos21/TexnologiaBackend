a70f65a7dcb263ca984a35730dcbe4da275c0ce8
Gzografos21 <63515820+Gzografos21@users.noreply.github.com>
2020-05-17 16:12:17 +0300

    Import Changes

---
+++ Map.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class Map
+    {
+        private string tripDestination;
+        private string tripDate;
+        private bool pinPhoto;
+    }
+
+    //an kai o xartis afora to front end sto sugekrimeno sustima tha xreiastoume na apothikeusoume ta pins sti vasi
+    //mporoun na xrisimopoiithoun diafora ergaleia opws ajax gia tin emfanisi twn pin panw sto xarti xwris na ginetai refresh tis selidas
+
+    public bool savePin()//me ta antistoixa orismata
+    {
+        //se auto to simeio tha prepei na pairnoume ta coordinates tou kathe pin pou vazei o xristis kai na ta apothikeuoume me insert sti vasi dedomenwn se ena kainourio table
+        //to table auto tha mporouse na onomastei pins kai na apoteleitai apo to userid tou xristi kai alles treis stiles lattitude kai longitude photo kai imerominia taxidiou
+        //kai otan mpainei o xristis sto profile tou to sustima tha prepei automata na kanei select apo ti vasi dedomenwn ta stoixeia tou table pins gia paradeigma opws anaferame parapanw
+        //kai na ta emfanizei panw ston xarti
+        //epeidi omws mporei na exei polla stoixeia isws o kaluteros tropos einai na xrisimopoiisoume lista tis listas. I mia lista einai ta coordinates tis photos kai tin imerominia kai i alli lista einai to sunolo twn pins
+        //opote tha emfanizontai ola ta simeia ston xarti me tis fwtografies
+        //uparxoun texnologies etsi wste na mporei o xristis apla pigenontas to velaki panw apo to pin na tou emfanizei ti fwtografia, omws emvathunoumai parapanw apo oso apaiteitai
+        //epomenes merikes endeiktikes entoles gia tin apothikeusi pins sti vasi dedomenwn tha mporousan na einai
+        try
+        {
+            MySqlConnection cnn = new MySqlConnection(connectionString);
+            cnn.Open();
+            string sql = "INSERT INTO PINS (userID, latitude, longitude, date)VALUES('" + userid + "','" + lati + "','" + longi + "','"+date+"')";
+            MySqlCommand command = new MySqlCommand(sql, cnn);
+            command.ExecuteReader();
+            cnn.Close();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+}
+
+}
---
+++ Points.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendTexnologia
+{
+    public class Points
+    {
+        private int collectedPoints;
+        private int pointsForBackpack;
+        private int pointsForDiscountedHostel;
+        private int pointsForFreeTrip;
+        private int redeemedPoints;
+        private int remainingPointsToBackpack;
+        private int remainingPointsToDiscountedHostel;
+        private int remainingPointsToFreeTrip;
+
+        public bool increasePoints()
+        {
+            //methodos i opoia tha kaleitai etsi wste na sugentrwnei olous tous pontous apo kathe energeia pou kanei
+            //ousiastika i increasePoints tha einai dilwmeni se auti tin klasi kai tha kaleitai opou xreiazetai
+            //gia paradeigma an eimaste sti selida invite a friend, me to pou ginei h epilogi "send invitation" tha prepei na kaleitai
+            //i methodos increasePoints kai tha pairnei orismata pithanws diafora orismata opws tous sugekrimenous pontous gia kathe periptwsi
+            // to swma tis increasePoints tha apoteleitai sigoura apo delete kai insert gia ti vasi dedomenwn
+            //kai mporei na mpei sto table Users mia stili me tous pontous tou xristi kai kathe fora pou tha kaleitai auti i methodos
+            // kanontas praxeis mesa stin methodo tha anoigei ti vasi tha kanei delete tous proigoumenous pontous tou xristi kai insert sti sugekrimeni stili kai grammi (diladi me vasi to userid tou xristi)
+            //tous kainourious pontous tou xristi 
+            //episis kai se autin tin periptwsi tha prepei na xrisimopoiithei to session gia na perasoun to user id etsi wste na xeroume gia poion xristi sugekrimena kanoume insert kai delete tous pontous tou
+
+        }
+
+        public bool checkPoints()
+        {
+            //edw tha ginetai select apo ti vasi gia na vriskoume akrivws ta points tou sugekrimenou xristi
+        }
+
+        public string displayRedeemChoices()
+        {
+            //select apo ti vasi gia emfanisi twn epilogwn exargurwsis
+            //analoga me tous pontous pou exei sugentrwsei o xristis gia to backpack kai tis ekptwseis
+        }
+
+        public string substractPoints()
+        {
+            //edw tha afairei tous exargurwmenous pontous
+        }
+
+        public static void bookTravelPackage()
+        {
+            //ton pigenei stin othoni kleisimatos taxidiwtikou paketou, otan epilexei na kleisei taxidiwtiko paketo
+        }
+
+        public static void inviteAFriend()
+        {
+            //ton pigenei stin othoni prosklisis filou, otan epilexei na proskalesei filo 
+        }
+
+        public static void reservationToFiloxenia()
+        {
+            //ton pigenei stin othoni kratisis dwmatiou sto Filoxenia
+        }
+
+        public static void uploadToFiloxenia()
+        {
+            //othoni anartisis dwmatiou sto Filoxenia
+        }
+
+        public static void bookHostel()
+        {
+            //othoni kratisis Hostel
+        }
+
+        public static void bookTickets()
+        {
+            //othoni kratisis eisitiriwn
+        }
+
+        public static void post()
+        {
+            //othoni dimosieusis taxidiwtikou periexomenou
+        }
+
+    }
+
+}
