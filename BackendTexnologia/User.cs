e78265b23e0af33f2d69c283f7cd922085ab8f8d
Gzografos21 <63515820+Gzografos21@users.noreply.github.com>
2020-05-17 16:23:48 +0300

    Import Changes

--- BackendTexnologia/User.cs
+++ BackendTexnologia/User.cs
@@ -12,5 +12,20 @@ namespace BackendTexnologia
         private string password;
         private string dateofbirth;
         private string phone;
+
+        //tha prepei na ginei sundesi me ti vasi 
+        //kai tha uparxei pali try catch se periptwsi pou skasei error sti vasi 
+        //sto epomeno paradoteo tha perasoume pio analutika neous methodous
+        //gia olous tous pithanous elegxous pou xreiazontai gia na ginoun kata tin eggrafi ii ti sundesi tou xristi
+
+        public bool signUp()
+        {
+            //tsekaroume an egine to signup kai epistrefoume true i false
+            
+        }
+        public bool login()
+        {
+            //tsekaroume an egine to login kai epistrefoume true i false
+        }
     }
 }
